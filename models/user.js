2c0b504ceb3a2d31d8a07ed272c8ac03772587e3
Kate Wood <katewood611@gmail.com>
2015-11-18 11:55:21 -0800

    adds pre method to the modle to salt password with bcrypt prior to saving a new user to the database

--- models/user.js
+++ models/user.js
@@ -1,10 +1,19 @@
 var mongoose 	= require('mongoose'),
 	Schema 		= mongoose.Schema,
-	bcrypt 		= require('bcrypt-node'),
+	bcrypt 		= require('bcrypt'),
 	userSchema  = new Schema({
 		name: String,
 		email: {type: String, required: true},
 		password: {type: String, required: true}
 	})
 
-module.exports = mongoose.model('User', userSchema)
+userSchema.pre('save', function(next) {
+	var user = this,
+		hash = bcrypt.hashSync(user.password, 8)
+	user.password = hash
+	next()
+})
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
