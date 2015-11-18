60ab777ed15e2da856a9d183d69357da97ce6dab
Kate Wood <katewood611@gmail.com>
2015-11-18 12:24:07 -0800

    refactors controller functions

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,35 +3,25 @@ var User = require('../models/user.js')
 function create(req, res) {
 	var user = new User(req.body.user)
 	user.save(function(err) {
-		if (err) {
-			res.json({err: err})
-		} else {
-			res.json({message: 'User created'})
-		}
+		if (err) res.json({err: err})
+		res.json({message: 'User created'})
 	})
 }
 
 function show(req, res) {
 	User.findById(req.params.id, function(err, user) {
-		if (err) {
-			res.json({err: err})
-		} else {
-			res.json(user)
-		}
+		if (err) res.json({err: err})
+		res.json(user)
 	})
 }
 
 function signIn(req, res) {
 	User.findOne({email: req.body.email}, function(err, user) {
-		if (err) {
-			res.json({err: err})
-		} else {
-			if (user.authenticate(req.body.password)) {
-				res.json(user)
-			} else {
-				res.json({message: 'Wrong password :('})
-			}
-		}
+		if (err) res.json({err: err})
+		if (user.authenticate(req.body.password))
+			res.json({message: 'Welcome, ' + user.name, user: user})
+		else
+			res.json({message: 'Wrong password :('})
 	})
 }
 
